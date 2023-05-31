0fbdabc3f58c89a91fa6d3015d803f829ebd782c
yuritakagi <takagiyuri0712@gmail.com>
2023-05-31 16:47:30 +0900

    add: postのマイグレーションを作成

--- src/database/migrations/2023_05_31_055829_create_posts_table.php
+++ src/database/migrations/2023_05_31_055829_create_posts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('response');
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }

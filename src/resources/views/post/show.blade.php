9f1ccb2a863e65a67b41ffab1fc50d75c5c554e0
yuritakagi <takagiyuri0712@gmail.com>
2023-06-04 14:43:03 +0900

    add: 画像を表示するよう追記

--- src/resources/views/post/show.blade.php
+++ src/resources/views/post/show.blade.php
@@ -11,6 +11,7 @@
         <div class="max-w-7xl mx-auto pb-3 sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <img src="{{ isset($post->user->profile_image) ? asset('storage/' . $post->user->profile_image) : asset('images/user_icon.png') }}" class="w-16 h-16 rounded-full object-cover border-none bg-gray-200">
                     {{ $post->user->name }}
                     {{ $post->created_at }}
                 </div>
@@ -20,12 +21,15 @@
                 </div>
                 <div class="p-6">
                     <div class="flex justify-end space-x-4">
-                        <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">お気に入り</a>
+                        @if ($post->user->id === Auth::user()->id)
                         <a href="{{ route('post.edit', ['post' => $post->id]) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">編集</a>
                         <form action="{{ route('post.destroy', ['post' => $post->id]) }}" method="post">
                             @csrf
                             <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">削除</button>
                         </form>
+                        @else
+                        <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">お気に入り</a>
+                        @endif
                     </div>
                 </div>
             </div>

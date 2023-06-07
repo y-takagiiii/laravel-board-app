70ed8990cae38dd14c3890c7336a74f779ca89d4
yuritakagi <takagiyuri0712@gmail.com>
2023-06-07 22:48:55 +0900

    add: ユニーク制約を追加

--- src/database/migrations/2023_06_07_185409_create_favorites_table.php
+++ src/database/migrations/2023_06_07_185409_create_favorites_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('post_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->unique('user_id', 'post_id');
             $table->timestamps();
         });
     }

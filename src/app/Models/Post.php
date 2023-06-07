5b31c2c1b58646fbbb3b257863775b8517b80819
yuritakagi <takagiyuri0712@gmail.com>
2023-06-07 19:15:01 +0900

    add: アソシエーションを追記

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -18,4 +18,8 @@ class Post extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function favorites() {
+        return $this->hasMany(Favorite::class);
+    }
 }

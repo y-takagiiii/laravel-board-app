ca4961fcef99affe2eba53d8e4325a328cee4414
yuritakagi <takagiyuri0712@gmail.com>
2023-06-07 19:14:56 +0900

    add: アソシエーションを追記

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
     public function posts() {
         return $this->hasMany(Post::class);
     }
+
+    public function favorites() {
+        return $this->hasMany(Favorite::class);
+    }
 }

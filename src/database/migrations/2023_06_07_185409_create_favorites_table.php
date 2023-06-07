fbf17ec67016283568712a0641fa1511317b0573
yuritakagi <takagiyuri0712@gmail.com>
2023-06-07 19:03:31 +0900

    add: マイグレーションを編集

--- src/database/migrations/2023_06_07_185409_create_favorites_table.php
+++ src/database/migrations/2023_06_07_185409_create_favorites_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
             $table->timestamps();
         });
     }
